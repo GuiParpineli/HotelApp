522984bd67f740936607294e6c79ada5c09cb236
Gui Parpineli <guiparpineli@GuiPC>
2022-11-14 12:57:33 -0300

    add repository and service

--- app/src/main/java/com/example/fragments/model/Hotel.kt
+++ app/src/main/java/com/example/fragments/model/Hotel.kt
@@ -1,10 +1,10 @@
 package com.example.fragments.model
 
 data class Hotel(
-    val id: Long = 0,
-    val name: String = "",
-    val address: String = "",
-    val rating: Float = 0.0F
+    var id: Long = 0,
+    var name: String = "",
+    var address: String = "",
+    var rating: Float = 0.0F
 ) {
     override fun toString(): String = name
 }
--- app/src/main/java/com/example/fragments/repository/HotelRepository.kt
+++ app/src/main/java/com/example/fragments/repository/HotelRepository.kt
@@ -4,8 +4,8 @@ import com.example.fragments.model.Hotel
 import javax.security.auth.callback.Callback
 
 interface HotelRepository {
-    fun save(hotal: Hotel)
-    fun remove(vararg hotel: Hotel)
+    fun save(hotel: Hotel)
+    fun remove(vararg hotels: Hotel)
     fun hotelById(id: Long, callback: (Hotel?) -> Unit)
     fun search(term: String, callback: (List<Hotel>) -> Unit)
 }
